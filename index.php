053e899ecd8e57ae2c67aa8302917e2600acfc8b
Kami Ⓒ <ramon.craeghs@student.kdg.be>
2021-03-09 15:36:27 +0100

    Versie 2

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include('layouts/header.php'); ?>
+
+<!-- Masthead-->
+        <header class="masthead">
+            <div class="container d-flex h-100 align-items-center">
+                <div class="mx-auto text-center">
+                    <h1 class="mx-auto my-0 text-uppercase">week 4</h1>
+                    <h2 class="text-white-50 mx-auto mt-2 mb-5">Dit is een simpel voorbeeld om aan te tonen dat ik het concept van <strong>"includes"</strong> begrijp</h2>
+                    <h2 class="text-white-50 mx-auto mt-2 mb-5">Ik had geen website om dit mee aan te tonen dus heb ik een template site gebruikt</h2>
+                </div>
+            </div>
+        </header>
+
+<?php include('layouts/footer.php'); ?>
+
+
